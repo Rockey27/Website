9dfa78df35509d9f722e9985b953b10e297af8b6
Rockey27 <79752470+Rockey27@users.noreply.github.com>
2021-06-23 12:56:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-menu_list_array = ["Veg Margherita Pizza", "Peppy Paneer Pizza", " Chicken Dominator Pizza", "CHICKEN FIESTA Pizza", "Indi Chicken Tikka Pizza", "PEPPER BARBECUE CHICKEN Pizza", "PEPPER BARBECUE CHICKEN Pizza", "PEPPER BARBECUE & ONION Pizza", "Farm House Pizza", 
-                    ];
+menu_list_array = ["Veg Margherita Pizza", "Peppy Paneer Pizza", " Chicken Dominator Pizza", "CHICKEN FIESTA Pizza", "Indi Chicken Tikka Pizza", "PEPPER BARBECUE CHICKEN Pizza", "PEPPER BARBECUE CHICKEN Pizza", "PEPPER BARBECUE & ONION Pizza", "Farm House Pizza",]; 
+                    
 
 function getmenu(){
 var htmldata;
-//Complete the code
+htmldata="<ol class='menulist'> "
+menu_list_array.sort();
+for(var i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata+'<li>' + '</li>'
 }
-
+htmldata=htmldata+'<ol>' + '</ol>'
+document.getElementById("display_menu").innerHTML = htmldata; 
 function add_item(){
 var htmldata;
 var item=document.getElementById("add_item").value;
-//Complete the code
-
+menu_list_array.push(item);
+menu_list_array.sort();
+htmldata="<section class='cards'> "
+for(var i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata+'<li>' + '</li>'
 }
-
-function add_top(){
-//Complete the code
+htmldata=htmldata+'<div class="card>' + '<img src="images/pizzaImg.png"/> ' + menu_list_array[i]; 
+}
+htmldata=htmldata="</section>"
+document.getElementById("added_menu").innerHTML = htmldata;
 }
